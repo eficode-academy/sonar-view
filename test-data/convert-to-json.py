92e281b8c7dfee66268e3aca17e0c40ab0df9e71
suraj neupane <surajneupane@surajs-MacBook-Pro.local>
2020-04-02 17:25:31 +0300

    remove all empty items and I don't know field

--- test-data/convert-to-json.py
+++ test-data/convert-to-json.py
@@ -6,6 +6,8 @@ from random import randint
 current_dir = os.path.dirname(os.path.abspath(__file__)) 
 print(current_dir)                                       
 
+#Only run once to Fetch from CSV to JSON
+
 with open('test-data/large-test.csv') as f:
     reader = csv.DictReader(f)
     rows = list(reader)
@@ -19,6 +21,11 @@ with open('test-data/large-test.json', 'r') as data:
         if item['Name']:
             item['Name'] = "Sara Parker-"+str(randint(100, 999))
             item['Email Address'] = item['Name']+"@eficode.com"
-
+    for item in json_data:
+        for key in list(item.keys()):
+            if (item[key]=='' or item[key]=="Don't know it"):
+                del item[key]
+        
 with open('test-data/large-test.json', 'w') as f:
     json.dump(json_data, f)
+
