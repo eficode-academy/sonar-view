45617c98e38b015d3003b050600d22bc45c6dd36
suraj neupane <surajneupane@surajs-MacBook-Pro.local>
2020-04-02 15:22:31 +0300

    Test data ready converted from csv to json with dummy name and email

--- README.md
+++ README.md
@@ -10,3 +10,15 @@ Frontend queries the data for different things like:
 * Give me all data, sortet by survey period
 * Give me all data by this person, sortet by survey period
 * Give me all persons with this keyword in this level
+
+
+## Starting with project
+This project uses [venv](https://www.freecodecamp.org/news/manage-multiple-python-versions-and-virtual-environments-venv-pyenv-pyvenv-a29fb00c296f/)
+
+### Creating mock data
+This is the mock-data to work with frontend. The real app will replace this data from other source in future
+
+    cd sonar/
+    test-data/convert-to-json.py
+
+*data.json* file is created *under test-data*
--- test-data/convert-to-json.py
+++ test-data/convert-to-json.py
@@ -1,13 +1,24 @@
 import csv
 import json
 import os
+from random import randint
 
 current_dir = os.path.dirname(os.path.abspath(__file__)) 
 print(current_dir)                                       
 
-with open('test-data/test.csv') as f:
+with open('test-data/large-test.csv') as f:
     reader = csv.DictReader(f)
     rows = list(reader)
 
-with open('test-data/test.json', 'w') as f:
+with open('test-data/large-test.json', 'w') as f:
     json.dump(rows, f)
+
+with open('test-data/large-test.json', 'r') as data:
+    json_data = json.load(data) 
+    for item in json_data:
+        if item['Name']:
+            item['Name'] = "Sara Parker-"+str(randint(100, 999))
+            item['Email Address'] = item['Name']+"@eficode.com"
+
+with open('test-data/large-test.json', 'w') as f:
+    json.dump(json_data, f)
