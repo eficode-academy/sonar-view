aa25f945cbc23f6e406dfe1960ec70648531b6c1
Banafsheh Rahnama <banafsheh.rahnama@eficode.com>
2020-04-07 16:31:22 +0300

    basic styling

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import data from "../../test-data/large-test.json";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
+import Wrapper from "./containers/Wrapper";
+import theme from "./utils/theme";
 
 const Sonar = () => {
-  const renderNames = data.filter((item) => console.log(item.Name));
-  return <div>{renderNames}</div>;
+  const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: ${theme.font};
+    color: ${theme.colors.black};
+    * {
+      font-family: ${theme.font};
+      color: ${theme.colors.black};
+    }
+  } `;
+
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Wrapper />
+      </ThemeProvider>
+    </BrowserRouter>
+  );
 };
 ReactDOM.render(<Sonar />, document.getElementById("app"));
