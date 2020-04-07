9e174a267ca43558b2357f1c0d8aded1076afdb2
Banafsheh Rahnama <banafsheh.rahnama@eficode.com>
2020-04-07 16:30:57 +0300

    add theming

---
+++ frontend/src/utils/mediaQuery.js
@@ -0,0 +1,18 @@
+import { css } from "styled-components";
+
+const sizes = {
+  xs: 360,
+  s: 375,
+  md: 768,
+  l: 1366,
+  xl: 1920,
+};
+
+export default Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (max-width: ${sizes[label]}px) {
+      ${css(...args)};
+    }
+  `;
+  return acc;
+}, {});
---
+++ frontend/src/utils/theme.js
@@ -0,0 +1,27 @@
+import media from "./mediaQuery";
+
+const colors = {
+  black: "#101820",
+  yellow: "#FFD100",
+  lightGrey: "#D7D2Cb",
+  darkGrey: "#003E51",
+  green: "#008578",
+  blue: "#007FA3",
+};
+
+const fontSizes = {
+  small: "14px",
+  medium: "16px",
+  large: "20px",
+};
+
+const font = ["Montserrat, sans-serif"];
+
+const theme = {
+  colors,
+  media,
+  fontSizes,
+  font,
+};
+
+export default theme;
