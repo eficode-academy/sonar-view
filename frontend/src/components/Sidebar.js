38808d21913d054f6a0b8f54882eae04a66e7141
Banafsheh Rahnama <banafsheh.rahnama@eficode.com>
2020-04-19 23:02:33 +0300

    fetch and display data

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,39 +1,134 @@
-import React from "react";
+import React, { useState } from "react";
+import { compile } from "path-to-regexp";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import { Link, withRouter } from "react-router-dom";
 import { Flex, Box } from "rebass";
-import Link from "./Link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import NavLink from "./NavLink";
 import Logo from "./Logo";
 
 const Wrapper = styled.div`
   padding: 20px 40px;
-  width: 240px;
+  width: 280px;
   border-right: 1px solid ${(props) => props.theme.colors.lightGrey};
   height: 100vh;
   position: sticky;
   overflow: scroll;
+  transition: left 0.3s ease-in;
+  background-color: white;
+
   ${(props) => props.theme.media.md`
-      display: none
+  left: -100%;
+  width: 100%;
+  position: absolute;
+
+      ${
+        props.isOpen &&
+        `
+left: 0;
+
+      `
+      }
   `}
 `;
 
-function Sidebar({ items }) {
+const MenuButton = styled.button`
+  border: none;
+  background-color: transparent;
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  display: none;
+  ${({ theme }) => theme.media.md`
+       display: block;
+  `};
+`;
+
+const CloseButton = styled.button`
+  border: none;
+  background-color: transparent;
+  display: none;
+  ${({ isOpen, theme }) => isOpen && theme.media.md && `display: block`}
+`;
+// TODO: Rethink the side bar structure
+function Sidebar({ match, data, isLoading }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const urlPath = Object.keys(data).join("");
+
   return (
-    <Wrapper>
-      <Flex flexDirection="column">
-        <Box width="120px" mb="70px">
-          <Logo />
-        </Box>
-        {items.map((item) => (
-          <Link href={`/surveys/${item.id}`} title={item.date} />
-        ))}
-      </Flex>
-    </Wrapper>
+    <>
+      <MenuButton onClick={() => setIsOpen(true)}>
+        <FontAwesomeIcon icon={faBars} as="button" />
+      </MenuButton>
+      <Wrapper isOpen={isOpen}>
+        <Flex flexDirection="column">
+          <Flex
+            width={["100%", "100%", "120px"]}
+            mb="70px"
+            justifyContent="space-between"
+          >
+            <Link to="/">
+              <Box width="120px">
+                <Logo />
+              </Box>
+            </Link>
+            <CloseButton onClick={() => setIsOpen(false)} isOpen={isOpen}>
+              <FontAwesomeIcon icon={faTimes} />
+            </CloseButton>
+          </Flex>
+          {!isLoading &&
+            data[urlPath]?.map((item, i) => {
+              let pathToPage = "";
+              const setPath = compile(match.path);
+
+              if (match.params.email) {
+                const newPath = setPath({
+                  ...match.params,
+                  email: item.email,
+                  date: match.params.date,
+                });
+
+                pathToPage = newPath;
+              }
+              if (!match.params.email && !match.params.date) {
+                pathToPage = `/${urlPath}/${item[i]}`;
+              } else if (match.params.date && !match.params.email) {
+                pathToPage = `${match.url}/${item.email}`;
+              }
+
+              return (
+                <NavLink
+                  href={pathToPage}
+                  title={item.name || item[i]}
+                  key={item.name || item[i]}
+                  onClick={() => setIsOpen(false)}
+                />
+              );
+            })}
+        </Flex>
+      </Wrapper>
+    </>
   );
 }
 
-Sidebar.propTypes = {
-  items: PropTypes.arrayOf(PropTypes.string).isRequired,
+Sidebar.defaultProps = {
+  isLoading: true,
 };
 
-export default Sidebar;
+Sidebar.propTypes = {
+  isLoading: PropTypes.bool,
+  data: PropTypes.objectOf(PropTypes.any).isRequired,
+  match: PropTypes.shape({
+    path: PropTypes.string,
+    url: PropTypes.string,
+    params: PropTypes.shape({
+      email: PropTypes.string,
+      date: PropTypes.string,
+    }),
+  }).isRequired,
+};
+
+export default withRouter(Sidebar);
--- frontend/src/containers/Wrapper.js
+++ frontend/src/containers/Wrapper.js
@@ -1,37 +1,41 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 import { Flex } from "rebass";
 import { Switch, Route } from "react-router-dom";
 import Home from "../components/Home";
 import Main from "../components/Main";
-import Sidebar from "../components/Sidebar";
-import data from "../../testjson.json";
+import Person from "../components/Person";
+import SidebarWrapper from "./SidebarWrapper";
 
-const Wrapper = () => {
-  const dates = data.map((item) => {
-    return {
-      date: item.survey_date,
-      id: item.id,
-    };
-  });
-
-  const renderUsers = (router) => {
-    const { id } = router.match.params;
-    const day = data.filter((days) => days.id === id);
-    return <Main data={day[0]} />;
-  };
-
+export const Layout = ({ component: Component, route }) => {
   return (
     <Flex width="100%" sx={{ height: "100vh" }}>
-      <Sidebar items={dates} />
-      <Flex alignItems="center" justifyContent="center" m="auto">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/surveys/:id" render={renderUsers} />
-        </Switch>
+      <SidebarWrapper />
+      <Flex alignItems="center" justifyContent="center" m="auto" width="100%">
+        <Component route={route} />
       </Flex>
     </Flex>
   );
 };
+
+Layout.propTypes = {
+  route: PropTypes.objectOf(PropTypes.any).isRequired,
+  component: PropTypes.func.isRequired,
+};
+
+function Wrapper() {
+  const layoutRender = (component) => (route) => (
+    <Layout component={component} route={route} />
+  );
+
+  return (
+    <Switch>
+      <Route path="/" exact render={layoutRender(Home)} />
+      <Route exact path="/surveys/:date" render={layoutRender(Main)} />
+      <Route path="/surveys/:date/:email" render={layoutRender(Person)} />
+      <Route render={() => <h1>404: page not found</h1>} />
+    </Switch>
+  );
+}
 
 export default Wrapper;
